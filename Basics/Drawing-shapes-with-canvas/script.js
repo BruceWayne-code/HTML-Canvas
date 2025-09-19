41c6eba2f4917d16afb455641cb4cf5499e2bb80
YOGESH SHARMA <yogeshsharma451950@gmail.com>
2025-09-19 09:30:46 +0530

    Update script.js

--- Basics/Drawing-shapes-with-canvas/script.js
+++ Basics/Drawing-shapes-with-canvas/script.js
@@ -1,1 +1,11 @@
-
+function draw(){
+    const canvas = document.     getElementById('main-canvas'
+    )
+    if(canvas.getContext){
+        const ctx = canvas.getContext('2d')
+    }
+    else{
+        //fallback Content here
+    }   
+}
+draw()
