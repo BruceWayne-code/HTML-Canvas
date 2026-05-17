568a411201314209394fb1a1618ea30096c39176
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-17 13:22:08 +0530

    feat: [KAN-4] add numberPhaseTesting variable

--- Basics/game/testing.js
+++ Basics/game/testing.js
@@ -2,6 +2,8 @@ console.log('Checking testing....')
 try{
   let s = 'check ticket distrubting in jira'
   console.log('checking now = ',s)
+  let numberPhaseTesting = 2323;
+  
 }catch(error){
   console.error('Error occur during checking ticket in jira')
 }
