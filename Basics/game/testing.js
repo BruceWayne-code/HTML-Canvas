022bb7e5902493ece575c9fba20a8582347ebd9d
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-17 13:12:54 +0530

    feat: Add extension numbering test

--- Basics/game/testing.js
+++ Basics/game/testing.js
@@ -2,6 +2,8 @@ console.log('Checking testing....')
 try{
   let s = 'check ticket distrubting in jira'
   console.log('checking now = ',s)
+  let anotherTestPhase = 2323;
+  console.log('Extension numbering testing = ')
 }catch(error){
   console.error('Error occur during checking ticket in jira')
 }
