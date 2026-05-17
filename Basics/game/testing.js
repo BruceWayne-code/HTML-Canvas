5105b55c64c0a8cf86c62e2b5bb99c0686dc9516
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-17 13:22:26 +0530

    feat(game): [KAN-4] Add console log to testing.js

--- Basics/game/testing.js
+++ Basics/game/testing.js
@@ -3,7 +3,7 @@ try{
   let s = 'check ticket distrubting in jira'
   console.log('checking now = ',s)
   let numberPhaseTesting = 2323;
-  
+  console.log('check number phase testing = ',numberPhaseTesting)
 }catch(error){
   console.error('Error occur during checking ticket in jira')
 }
