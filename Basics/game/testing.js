cb977e2fa95bbe523ca73ac006d35016d970c80f
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-18 10:03:01 +0530

    fix: [KAN-4] log rs error in testing.js

--- Basics/game/testing.js
+++ Basics/game/testing.js
@@ -4,6 +4,7 @@ try{
   console.log('checking now = ',s)
   let numberPhaseTesting = 2323;
   let rs = 232
+  console.error('rs error = ',rs)
   console.log('check number phase testing = ',numberPhaseTesting)
 }catch(error){
   console.error('Error occur during checking ticket in jira')
