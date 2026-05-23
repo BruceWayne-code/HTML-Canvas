4c8d6d83f3106177e69521c53d3db279a04d6270
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-23 13:14:04 +0530

    chore(game): [KAN-4] add testing.js to aiignore

--- Basics/game/check.py
+++ Basics/game/check.py
@@ -1,2 +1,3 @@
 print('python successfully installed')
 print('test1 for testing aiignore files with changnging data now staging changes and getting chore automated changes')
+print('now testing the phase of aiignore step where ')
