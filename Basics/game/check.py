63786d97ac38a648a16903d79751d30572ea3758
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-23 13:11:41 +0530

    chore(game): [KAN-4] update game check script

--- Basics/game/check.py
+++ Basics/game/check.py
@@ -1,2 +1,2 @@
 print('python successfully installed')
-print('test1 for testing aiignore files with changnging data')
+print('test1 for testing aiignore files with changnging data now staging changes and getting chore automated changes')
