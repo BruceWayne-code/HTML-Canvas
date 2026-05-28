2526ef04c983f3c635ce52739f94b50f2a631ce2
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-28 12:18:39 +0530

    chore(game): [KAN-4] add redraw function and print statements

--- Basics/game/check.py
+++ Basics/game/check.py
@@ -9,3 +9,14 @@ print('lets check again and again')
 function redraw() {
   //djsjdd
   drawPlayer();}
+  print('python successfully installed')
+print('test1 for testing aiignore files with changnging data now staging changes and getting chore automated changes')
+# hnpew
+print('is it work')
+print('i think it or not be probably')
+print('lets check again and again')
+print('i think it or not be probably')
+print('lets check again and again')
+function redraw() {
+  //djsjdd
+  drawPlayer();}
