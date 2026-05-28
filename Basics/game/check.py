e1f2bd1d729b4989f470465e3ee69c0bf0e4e808
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-28 12:23:02 +0530

    chore(game): remove unused print statement and ejs template

--- Basics/game/check.py
+++ Basics/game/check.py
@@ -12,16 +12,3 @@ function redraw() {
   print('python successfully installed')
 print('test1 for testing aiignore files with changnging data now staging changes and getting chore automated changes')
 # hnpew
-print('is it work')
-<%- include('../partials/head.ejs') %>
-  <body class="w-screen h-screen">
-      <%- include('../partials/headerNav.ejs')%>
-      <main class="w-full h-5/6 flex flex-col justify-center items-center ">
-          <section class="w-fit h-fit px-8 py-4 bg-gray-300 text-red-600  rounded-xl">
-            <h1 class="text-center text-6xl mb-4"><output id="error-code">404</output></h1>
-            <h2 class="text-center text-4xl mb-4">Page not found</h2>
-          </section>
-      </main>
-  </body>
-</html>
- 
