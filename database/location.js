9068f24a2642d1cb917ec8ef5fb2dc4d5da9749e
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-28 12:28:49 +0530

    refactor(db): [KAN-4] remove unused aiIgnores from location.js

--- database/location.js
+++ database/location.js
@@ -1,6 +1,5 @@
 exports.getIgnorePatterns=()=>{
     let pattern =[];
     const defaultIgnore = vscode.workspace.getConfiguration('aiGitFlowAgent').defaultAiIgnore
-    const aiIgnores = Path2D.join(this.getIgnorePatterns,'.aiignore')
     return patterns = [...defaultIgnore]
 }
