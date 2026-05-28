e5cc5e950c0296ea8b8a3a016b25e3dfc816c882
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-28 12:13:41 +0530

    chore(game): [KAN-4] add debug print statement

--- Basics/game/check.py
+++ Basics/game/check.py
@@ -2,3 +2,4 @@ print('python successfully installed')
 print('test1 for testing aiignore files with changnging data now staging changes and getting chore automated changes')
 # hnpew
 print('is it work')
+print('i think it')
