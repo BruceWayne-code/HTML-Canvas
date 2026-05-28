27d65ceb3602d967d5db01d1b63edc254a07a9e4
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-28 12:28:00 +0530

    refactor(db): [KAN-4] update location.js for database saving

--- database/location.js
+++ database/location.js
@@ -1,5 +1,6 @@
-exports.getIgnorePatterns(){
+exports.getIgnorePatterns=()=>{
     let pattern =[];
     const defaultIgnore = vscode.workspace.getConfiguration('aiGitFlowAgent').defaultAiIgnore
+    const aiIgnores = Path2D.join(this.getIgnorePatterns,'.aiignore')
     return patterns = [...defaultIgnore]
 }
