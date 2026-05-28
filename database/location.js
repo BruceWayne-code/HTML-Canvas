2840dce2041d6774fbdf868d9d166d713c25b328
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-05-28 12:26:37 +0530

    chore(db): [KAN-4] fix missing parenthesis in getIgnorePatterns

--- database/location.js
+++ database/location.js
@@ -1,4 +1,4 @@
-exports.getIgnorePatterns=>(){
+exports.getIgnorePatterns(){
     let pattern =[];
     const defaultIgnore = vscode.workspace.getConfiguration('aiGitFlowAgent').defaultAiIgnore
     return patterns = [...defaultIgnore]
