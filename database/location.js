1173edd4cd3757459597af2635bbb54bba80cbdd
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-06-03 13:31:34 +0530

    chore: automated changes

--- database/location.js
+++ database/location.js
@@ -1,1 +1,1 @@
-let apiKey = 'vscode.window.showErrorMessage(`${error.message || error}'+"await updateSTatusBarText('Error', '$(error)')"
+let apiKey = 'check again vscode.window.showErrorMessage(`${error.message || error}'+"await updateSTatusBarText('Error', '$(error)')"
