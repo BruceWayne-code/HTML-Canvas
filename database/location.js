0d68e6e99d60ec28526ac41b6f5ac1755418e27d
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-06-03 21:56:08 +0530

    add api keys of multiples database

--- database/location.js
+++ database/location.js
@@ -1,1 +1,6 @@
 let apiKey = 'check again and now login is write to fetch the error try again can it make same thing vscode.window.showErrorMessage(`${error.message || error}'+"await updateSTatusBarText('Error', '$(error)')"
+let apiKey2 = 'check again and now login is write to fetch the error try again can it make same thing vscode.window.showErrorMessage(`${error.message || error}'+"await updateSTatusBarText('Error', '$(error)')"
+let apiKey3 = 'check again and now login is write to fetch the error try again can it make same thing vscode.window.showErrorMessage(`${error.message || error}'+"await updateSTatusBarText('Error', '$(error)')"
+let apiKey4 = 'check again and now login is write to fetch the error try again can it make same thing vscode.window.showErrorMessage(`${error.message || error}'+"await updateSTatusBarText('Error', '$(error)')"
+let apiKey5 = 'check again and now login is write to fetch the error try again can it make same thing vscode.window.showErrorMessage(`${error.message || error}'+"await updateSTatusBarText('Error', '$(error)')"
+let apiKey6 = 'check again and now login is write to fetch the error try again can it make same thing vscode.window.showErrorMessage(`${error.message || error}'+"await updateSTatusBarText('Error', '$(error)')"
