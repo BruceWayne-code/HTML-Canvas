8ab892c33c91d9a04bb5ed887307882bae93eb56
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-06-08 22:05:53 +0530

    chore(game): [KAN-4] remove unused setTimeout

--- Basics/game/script.js
+++ Basics/game/script.js
@@ -35,9 +35,6 @@ function drawPlayer() {
                 setTimeout(()=>{
                   state='error in changes';
                   printPlayer();
-                  setTimeout(()=>{
-                    printPlayer()
-                  },3000)
                 },3000)
                 setTimeout(()=>{
                   state = 'pending catching'
