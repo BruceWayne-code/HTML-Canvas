c15a910a778c983e8d501a60dee476c52d12e644
BruceWayne-code <yogeshsharma451950@gmail.com>
2026-06-27 11:20:02 +0530

    chore(database): [KAN-4] add log for location key

--- database/location.js
+++ database/location.js
@@ -19,4 +19,4 @@ let codeBlock =`function happy()=>{
     }`
 console.log('At the initial state your code must be like this : -',codeBlock)
 const key = a343490jsk;
-console.log('')
+console.log('Your key for opening ao')
